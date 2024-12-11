9c6f1669ea2496dfe8e38314cfa4e0a6bce4e1b2
Loai Rebouh <loai_rebouh@icloud.com>
2024-12-11 01:23:31 +0100

    refactor (delete button on donors list)

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,18 +5,18 @@ const Navbar: React.FC = () => {
 	const location = useLocation();
 
 	return (
-		<nav className="bg-blue-600 text-white p-4 flex items-center">
-			<ul className="flex flex-row gap-8 items-center mx-auto">
+		<nav className="bg-blue-950 text-white p-4 flex items-center">
+			<ul className="flex flex-row gap-8 items-center mx-auto text-3xl">
 				<li className={location.pathname === "/add-donor" ? "font-bold" : ""}>
 					<Link className="text-green-500" to="/add-donor">
-						Add Donor
+						New
 					</Link>
 				</li>
 				<li className={location.pathname === "/home" ? "font-bold" : ""}>
 					<Link to="/home">Home</Link>
 				</li>
 				<li className={location.pathname === "/donor-list" ? "font-bold" : ""}>
-					<Link to="/donor-list">Donor List</Link>
+					<Link to="/donor-list">Donors List</Link>
 				</li>
 			</ul>
 		</nav>
