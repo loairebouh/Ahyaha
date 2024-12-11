97d7b4bebbc6ebeb55de6fbfe84601a5133a50b5
Loai Rebouh <loai_rebouh@icloud.com>
2024-12-11 04:48:44 +0100

    refactor(tauri): install tauri

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import path from "path"
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
